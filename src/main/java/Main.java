30f09f2460cf6088c5299077bd2973a045b4e363
Aitooor <aitorarias19@gmail.com>
2023-02-16 20:11:45 +0100

    fix: extends JavaPlugin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
