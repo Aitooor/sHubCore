33430842abe5253774656a2cc6701d0132466393
Aitooor <aitorarias19@gmail.com>
2023-02-17 02:54:00 +0100

    fix: added more listeners to PlayerListeners

--- src/main/java/online/starsmc/hubcore/listeners/PlayerListeners.java
+++ src/main/java/online/starsmc/hubcore/listeners/PlayerListeners.java
@@ -1,11 +1,18 @@
 package online.starsmc.hubcore.listeners;
 
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.SpawnCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 
 public class PlayerListeners implements Listener {
 
@@ -29,5 +36,43 @@ public class PlayerListeners implements Listener {
         if(player.getLocation().getY() < 0) {
             player.teleport(player.getLocation()); //Change to spawn point
         }
+    }
+
+    @EventHandler
+    public void onFoodLevelChangeEvent(FoodLevelChangeEvent event) {
+        Entity player = event.getEntity();
+
+        if(player instanceof Player) {
+            event.setCancelled(true);
+            event.setFoodLevel(20);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageEvent(EntityDamageEvent event) {
+        Entity player = event.getEntity();
+
+        if(player instanceof Player) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler
+    public void onWorldLoadEvent(WorldLoadEvent event) {
+        World world = event.getWorld();
+
+        world.setTime(3600);
+        world.setStorm(false);
+        world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        world.setGameRule(GameRule.FALL_DAMAGE, false);
+        world.setSpawnLimit(SpawnCategory.AMBIENT, 0);
+        world.setSpawnLimit(SpawnCategory.ANIMAL, 0);
+        world.setSpawnLimit(SpawnCategory.MONSTER, 0);
+        world.setSpawnLimit(SpawnCategory.MISC, 0);
+        world.setSpawnLimit(SpawnCategory.AXOLOTL, 0);
+        world.setSpawnLimit(SpawnCategory.WATER_AMBIENT, 0);
+        world.setSpawnLimit(SpawnCategory.WATER_ANIMAL, 0);
+        world.setSpawnLimit(SpawnCategory.WATER_UNDERGROUND_CREATURE, 0);
     }
 }
