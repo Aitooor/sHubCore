c302913e1f663ea4a0b8b920fed47e5cb8dd2b28
Aitooor <aitorarias19@gmail.com>
2023-02-21 18:39:13 +0100

    feat: added User manager to Inject

--- src/main/java/online/starsmc/hubcore/module/PluginModule.java
+++ src/main/java/online/starsmc/hubcore/module/PluginModule.java
@@ -6,6 +6,7 @@ import online.starsmc.hubcore.server.manager.ServerGameManager;
 import online.starsmc.hubcore.server.manager.ServerHubManager;
 import online.starsmc.hubcore.service.CommandService;
 import online.starsmc.hubcore.service.Service;
+import online.starsmc.hubcore.user.UserManager;
 import team.unnamed.inject.AbstractModule;
 
 public class PluginModule extends AbstractModule {
@@ -13,6 +14,7 @@ public class PluginModule extends AbstractModule {
         private final ServerHubManager serverHubManager = new ServerHubManager();
         private final ServerGameManager serverGameManager = new ServerGameManager();
         private final ScoreboardManager scoreboardManager = new ScoreboardManager();
+        private final UserManager userManager = new UserManager();
 
         public PluginModule(Main plugin) {
             this.plugin = plugin;
@@ -24,6 +26,7 @@ public class PluginModule extends AbstractModule {
             bind(ServerHubManager.class).toInstance(serverHubManager);
             bind(ServerGameManager.class).toInstance(serverGameManager);
             bind(ScoreboardManager.class).toInstance(scoreboardManager);
+            bind(UserManager.class).toInstance(userManager);
 
             multibind(Service.class).asSet().to(CommandService.class);
 
--- src/main/java/online/starsmc/hubcore/user/UserManager.java
+++ src/main/java/online/starsmc/hubcore/user/UserManager.java
@@ -3,15 +3,12 @@ package online.starsmc.hubcore.user;
 import online.starsmc.hubcore.model.repository.CachedModelRepository;
 import online.starsmc.hubcore.server.ServerModel;
 
+import javax.inject.Inject;
 import java.util.UUID;
 
 public class UserManager {
 
-    private final CachedModelRepository<UserModel> cachedRepository;
-
-    public UserManager(CachedModelRepository<UserModel> cachedRepository) {
-        this.cachedRepository = cachedRepository;
-    }
+    @Inject private CachedModelRepository<UserModel> cachedRepository;
 
     public boolean canAccessServer(UUID userId, ServerModel serverModel) throws Exception {
         UserModel userModel = cachedRepository.getOrFindAndCache(userId.toString());
