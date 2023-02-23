517984107371c213d0ee2e3f56e02781d8143655
Aitooor <aitorarias19@gmail.com>
2023-02-23 18:35:42 +0100

    fix: added to all Inject

--- src/main/java/online/starsmc/hubcore/module/PluginModule.java
+++ src/main/java/online/starsmc/hubcore/module/PluginModule.java
@@ -4,19 +4,16 @@ import online.starsmc.hubcore.Main;
 import online.starsmc.hubcore.bungee.BungeeManager;
 import online.starsmc.hubcore.model.Model;
 import online.starsmc.hubcore.model.repository.CachedModelRepository;
-import online.starsmc.hubcore.server.ServerModel;
 import online.starsmc.hubcore.server.manager.ServerGameManager;
 import online.starsmc.hubcore.server.manager.ServerHubManager;
 import online.starsmc.hubcore.service.CommandService;
 import online.starsmc.hubcore.service.Service;
 import online.starsmc.hubcore.user.UserManager;
-import online.starsmc.hubcore.user.UserModel;
 import team.unnamed.inject.AbstractModule;
 
 public class PluginModule extends AbstractModule {
         private final Main plugin;
-        private final CachedModelRepository<UserModel> usersCachedModelRepository = new CachedModelRepository<>();
-        private final CachedModelRepository<ServerModel> serversCachedModelRepository = new CachedModelRepository<>();
+        private final CachedModelRepository<Model> cachedModelRepository = new CachedModelRepository<>();
         private final ServerHubManager serverHubManager = new ServerHubManager();
         private final ServerGameManager serverGameManager = new ServerGameManager();
         private final UserManager userManager = new UserManager();
@@ -29,8 +26,7 @@ public class PluginModule extends AbstractModule {
         @Override
         protected void configure() {
             bind(Main.class).toInstance(plugin);
-            bind(CachedModelRepository.class).toInstance(usersCachedModelRepository);
-            bind(CachedModelRepository.class).toInstance(serversCachedModelRepository);
+            bind(CachedModelRepository.class).toInstance(cachedModelRepository);
 
             bind(ServerHubManager.class).toInstance(serverHubManager);
             bind(ServerGameManager.class).toInstance(serverGameManager);
