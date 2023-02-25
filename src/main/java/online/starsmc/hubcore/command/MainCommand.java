5c33b97306a0b91937ad0bde21c022a4026c662e
Aitooor <aitorarias19@gmail.com>
2023-02-25 03:40:02 +0100

    feat: setspawn and spawn administrator commands

--- src/main/java/online/starsmc/hubcore/command/MainCommand.java
+++ src/main/java/online/starsmc/hubcore/command/MainCommand.java
@@ -3,14 +3,20 @@ package online.starsmc.hubcore.command;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import online.starsmc.hubcore.Main;
 import online.starsmc.hubcore.utils.ChatUtil;
+import online.starsmc.hubcore.utils.codec.LocationCodec;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import team.unnamed.inject.InjectAll;
 
 @InjectAll
-@Command(names = {"hubcore", "sHubCore", "sHubcore", "shubcore", "shubCore"})
+@Command(names = {"hubcore", "sHubCore", "sHubcore", "shubcore", "shubCore"}, permission = "hubcore.admin")
 public class MainCommand implements CommandClass {
+
+    private Main plugin;
     @Command(names = {"reload", "rl"}, permission = "hubcore.reload")
     public void reloadCommand(@Sender CommandSender sender){
         if(!(sender instanceof Player)) {
@@ -20,7 +26,17 @@ public class MainCommand implements CommandClass {
         ChatUtil.sendMsgSenderPrefix(sender, "&cNot done yet");
     }
     @Command(names = "setspawn", permission = "hubcore.setspawn")
-    public void setSpawn(@Sender Player player, String arg){
-        ChatUtil.sendMsgSender(player, "Not done yet");
+    public void setSpawn(@Sender Player player) {
+        plugin.getConfig().set("spawn_location", LocationCodec.serialize(player.getLocation()));
+        plugin.saveConfig();
+        plugin.reloadConfig();
+
+        ChatUtil.sendMsgSender(player, "&aSpawn correctly set");
+    }
+
+    @Command(names = "spawn", permission = "hubcore.spawn")
+    public void spawn(@Sender Player player) {
+        player.teleport(LocationCodec.deserialize(plugin.getConfig().getString("spawn_location")));
+        ChatUtil.sendMsgSender(player, "&aTeleported to spawn");
     }
 }
