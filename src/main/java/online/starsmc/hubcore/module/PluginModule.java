75fb941ecd0d0a401699c1f172ec1f191ccc4cdc
Aitooor <aitorarias19@gmail.com>
2023-02-25 02:39:41 +0100

    fix: removed user package

--- src/main/java/online/starsmc/hubcore/module/PluginModule.java
+++ src/main/java/online/starsmc/hubcore/module/PluginModule.java
@@ -14,9 +14,6 @@ import online.starsmc.hubcore.server.manager.ServerGameManager;
 import online.starsmc.hubcore.server.manager.ServerHubManager;
 import online.starsmc.hubcore.service.CommandService;
 import online.starsmc.hubcore.service.Service;
-import online.starsmc.hubcore.user.UserManager;
-import online.starsmc.hubcore.user.UserModel;
-import online.starsmc.hubcore.user.codec.UserJsonCodec;
 import team.unnamed.inject.AbstractModule;
 
 import java.io.File;
@@ -25,32 +22,20 @@ import java.util.concurrent.TimeUnit;
 
 public class PluginModule extends AbstractModule {
         private final Main plugin;
-        private final CachedModelRepository<UserModel> userCachedModelRepository;
         private final CachedModelRepository<ServerModel> serverCachedModelRepository;
         private final CachedModelRepository<ServerModel> hubCachedModelRepository;
         private final ServerHubManager serverHubManager;
         private final ServerGameManager serverGameManager;
-        private final UserManager userManager;
         private final BungeeManager bungeeManager;
 
         public PluginModule(Main plugin) {
             File pluginFolder = plugin.getDataFolder();
             Path pluginPath = pluginFolder.toPath();
             Gson createGson = new GsonBuilder()
-                    .registerTypeAdapter(UserModel.class, new UserJsonCodec())
                     .registerTypeAdapter(ServerModel.class, new ServerJsonCodec())
                     .create();
 
             this.plugin = plugin;
-            this.userCachedModelRepository = new CachedModelRepository<>(
-                    new JsonModelRepository<>(pluginPath.resolve("users"), UserModel.class, createGson),
-                    new GuavaModelRepository<>(
-                            CacheBuilder.newBuilder()
-                                    .expireAfterAccess(10, TimeUnit.MINUTES)
-                                    .expireAfterWrite(10, TimeUnit.MINUTES)
-                                    .build()
-                    )
-            );
             this.serverCachedModelRepository = new CachedModelRepository<>(
                     new JsonModelRepository<>(pluginPath.resolve("servers"), ServerModel.class, createGson),
                     new GuavaModelRepository<>(
@@ -70,10 +55,9 @@ public class PluginModule extends AbstractModule {
                     )
             );
 
-            this.userManager = new UserManager(userCachedModelRepository);
             this.bungeeManager = new BungeeManager(plugin);
-            this.serverGameManager = new ServerGameManager(plugin, serverCachedModelRepository, userManager, bungeeManager);
-            this.serverHubManager = new ServerHubManager(plugin, hubCachedModelRepository, userManager, bungeeManager);
+            this.serverGameManager = new ServerGameManager(plugin, serverCachedModelRepository, bungeeManager);
+            this.serverHubManager = new ServerHubManager(plugin, hubCachedModelRepository, bungeeManager);
         }
 
         @Override
@@ -82,7 +66,6 @@ public class PluginModule extends AbstractModule {
 
             bind(ServerHubManager.class).toInstance(serverHubManager);
             bind(ServerGameManager.class).toInstance(serverGameManager);
-            bind(UserManager.class).toInstance(userManager);
             bind(BungeeManager.class).toInstance(bungeeManager);
 
             multibind(Service.class).asSet().to(CommandService.class);
