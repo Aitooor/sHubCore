deeebd3adde0b9668cd3a50de9c0d683576f02f9
Aitooor <aitorarias19@gmail.com>
2023-03-01 01:29:58 +0100

    feat: added Chat to config

--- src/main/java/online/starsmc/hubcore/listener/ChatListener.java
+++ src/main/java/online/starsmc/hubcore/listener/ChatListener.java
@@ -1,16 +1,34 @@
 package online.starsmc.hubcore.listener;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import online.starsmc.hubcore.Main;
+import online.starsmc.hubcore.utils.ChatUtil;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Objects;
+
 public class ChatListener implements Listener {
+
+    private final FileConfiguration config;
+
+    public ChatListener(Main plugin) {
+        this.config = plugin.getConfig();
+    }
 
     @EventHandler
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
-        event.getFormat();
-
-        //TODO Add to config
-        event.setFormat(event.getPlayer().getDisplayName() + ": " + event.getMessage());
+        Player player = event.getPlayer();
+        event.setFormat(ChatUtil.translate(
+                PlaceholderAPI.setPlaceholders(
+                        player,
+                        Objects.requireNonNull(config.getString("chat_format"))
+                                .replace("%player%", player.getDisplayName())
+                                .replace("%message%", event.getMessage())
+                )
+        ));
     }
 }
